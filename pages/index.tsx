064e783fb71609cf14ba57b90cc405f67e2d2f34
wonmin, lee <e2goon@gmail.com>
2020-09-08 02:02:52 +0900

    first commit :)
    - next + react + typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import Layout from "@/components/Layout";
+
+function Index(): ReactNode {
+  return (
+    <Layout>
+      <h1>RealWorld</h1>
+    </Layout>
+  );
+}
+
+export default Index;
