ed711f40bab69de9c5a87d6c8342609b2798e67c
wonmin, lee <e2goon@gmail.com>
2020-09-10 00:47:58 +0900

    chore: _app 코드 정리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,17 @@
  * https://github.com/vercel/next.js/tree/master/examples/with-emotion
  */
 import NextApp from "next/app";
+import { cache } from "emotion";
 import { CacheProvider } from "@emotion/core";
 import Layout from "@/components/Layout";
 
-// Use only { cache } from 'emotion'. Don't use { css }.
-import { cache } from "emotion";
 import "tailwindcss/dist/base.min.css";
-
-// import { globalStyles } from "../shared/styles";
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <CacheProvider value={cache}>
-        {/* {globalStyles} */}
         <Layout>
           <Component {...pageProps} />
         </Layout>
