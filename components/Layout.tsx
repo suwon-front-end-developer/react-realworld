84a91b456d7b1c2a147e6fc5a9474ec3d890bf26
wonmin, lee <e2goon@gmail.com>
2020-09-10 02:02:48 +0900

    update: 피드목록 목업 구현

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <>
       <Header />
-      <div>{children}</div>
+      <>{children}</>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
 import { ReactNode } from "react";
+import Head from "next/head";
 import Banner from "@/components/Banner";
+import FeedItem from "@/components/Feed/Item";
+import "twin.macro";
 
-function Index(): ReactNode {
+export default function Index(): ReactNode {
+  const feedList = new Array(10).fill("");
+  console.log(feedList);
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
       <Banner title="conduit" description="A place to share your knowledge." />
-      <h1>Home</h1>
+
+      <div tw="py-4 flex mx-auto max-w-screen-lg">
+        <div tw="px-4 flex-auto">
+          {feedList && (
+            <div role="list">
+              {feedList.map((val, idx) => (
+                <FeedItem
+                  key={idx}
+                  title="제목"
+                  description="내용"
+                  role="listitem"
+                />
+              ))}
+            </div>
+          )}
+        </div>
+
+        {/* Popular Tags 템플릿 컴포넌트 */}
+        <div tw="px-4 last:px-0 flex-none max-w-xs w-full">
+          <h2>Popular Tags</h2>
+        </div>
+      </div>
     </>
   );
 }
-
-export default Index;
