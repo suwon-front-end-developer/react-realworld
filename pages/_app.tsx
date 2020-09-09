ec6aefdbc3cb7518c46a056d32080f0f4be0ed2a
wonmin, lee <e2goon@gmail.com>
2020-09-10 00:51:24 +0900

    style: 웹폰트 적용 및 글로벌 스타일 선언

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
  * https://github.com/vercel/next.js/tree/master/examples/with-emotion
  */
 import NextApp from "next/app";
+import Head from "next/head";
 import { cache } from "emotion";
 import { CacheProvider } from "@emotion/core";
 import Layout from "@/components/Layout";
@@ -14,9 +15,25 @@ export default class App extends NextApp {
     const { Component, pageProps } = this.props;
     return (
       <CacheProvider value={cache}>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Source+Sans+Pro:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Titillium+Web:wght@700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+
+        {/* TODO: globalStyle 로 분류 */}
+        <style jsx global>{`
+          html {
+            font-family: -apple-system, BlinkMacSystemFont, "Source Sans Pro",
+              "Noto Sans KR", "Segoe UI", Roboto, "Helvetica Neue", Arial,
+              "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+              "Segoe UI Symbol", "Noto Color Emoji";
+          }
+        `}</style>
       </CacheProvider>
     );
   }
