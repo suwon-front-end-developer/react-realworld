a3791738a4d1844ed7bb0ed3a1ec3c1577b07db8
jade <jade@jades-MacBook-Pro.local>
2020-10-03 23:06:01 +0900

    add: Input 컴포넌트

---
+++ components/Input.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+import tw from "twin.macro";
+
+type Props = {
+  type: string;
+  placeholder?: string;
+  block: boolean;
+} & typeof defaultProps;
+
+const defaultProps = {
+  block: false,
+};
+
+const Input: FunctionComponent<Props> = ({ type, placeholder, block }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      css={[
+        tw`py-3 px-6 text-lg border rounded-md outline-none focus:border-blue-400`,
+        block && tw`block w-full`,
+      ]}
+    />
+  );
+};
+
+Input.defaultProps = defaultProps;
+
+export default Input;
