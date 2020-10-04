b941682e70b6f3d80fda39c154c945acbcb12b90
jade <jade@jades-MacBook-Pro.local>
2020-10-04 22:57:05 +0900

    add: LoginForm 템플릿 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,17 @@
+import "twin.macro";
 import { NextPage } from "next";
+import LoginForm from "templates/LoginForm";
 
 const LoginPage: NextPage = () => {
   return (
     <>
-      <h1>LoginPage</h1>
+      <div tw="py-8 px-4 mx-auto max-w-screen-lg">
+        <div tw="text-center">
+          <h1 tw="text-4xl">Sign in</h1>
+          <a href="#">Need an account?</a>
+        </div>
+        <LoginForm />
+      </div>
     </>
   );
 };
